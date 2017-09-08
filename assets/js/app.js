13ebc32f001797c14beed36f2a93e7575d223897
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-09-08 16:30:43 -0400

    finished functionality and styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-    // call jQuery sortable
-    $( function() {
-        $( ".sortable" ).sortable();
-        $( ".sortable" ).disableSelection();
-    });
-    
   // Initialize Firebase
     var config = {
         apiKey: "AIzaSyClg_9CBuyBx1zfG00NvXisqPkV1sSybZ4",
@@ -22,43 +16,72 @@ $(document).ready(function() {
 
     // User enters name
         var trainName = "";
-    
     // User enters Destination
         var trainDestination = "";
-    
     // User enters train Time
         var trainTime = 0;
-    
     // User enters frequency to determine time to destination
         var trainFrequency = 0;
-
+        var minAway = 0;
+        var key;
+        var nextTrainCoverted = 0;
+        var tMinutesTillTrain = 0;
+ 
     // Capture Train Submit Button Click
     $('#submit').on('click', function(){
         event.preventDefault();
 
         // store and retrieve the most recent train
-        trainName = $('#train-name').val().trim();
-        trainDestination = $('#train-destination').val().trim();
+        trainName = $('#train-name').val().trim().toProperCase();
+        trainDestination = $('#train-destination').val().trim().toProperCase();
         trainTime = $('#train-time').val().trim();
         trainFrequency = $('#train-frequency').val().trim();
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(trainTime, "hh:mm").subtract(1, "years");
+        console.log("firstTimeConverted: " + firstTimeConverted);
+
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % trainFrequency;
+        console.log("Remainder: " + tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = trainFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        var nextTrainCoverted = moment(nextTrain).format("hh:mm");
+        console.log('next train converted: ' + nextTrainCoverted);
 
         database.ref().push({
             trainName: trainName,
             trainDestination: trainDestination,
             trainTime: trainTime,
-            trainFrequency: trainFrequency
+            trainFrequency: trainFrequency,
+            nextTrainCoverted: nextTrainCoverted,
+            tMinutesTillTrain: tMinutesTillTrain,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
         }); // database set
-        printToPage();
+
     }); // submit on click
+
 
     function printToPage() {
         $('#train-schedule-area').empty();
-        var ref = firebase.database().ref();
-        var query = firebase.database().ref().orderByKey();
-        query.once("value")
-        .then(function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-            // key will be "ada" the first time and "alan" the second time
+        
+        database.ref().on("child_added", function(childSnapshot) {
+            // 
             var key = childSnapshot.key;
             console.log(key);
             // childData will be the actual contents of the child
@@ -69,26 +92,42 @@ $(document).ready(function() {
             console.log("train name: " + childData.trainName);
             console.log("train time: " + childData.trainTime);
             console.log("train Frequency: " + childData.trainFrequency);
+            console.log("nextTrainCoverted: " + childData.nextTrainCoverted);
 
-                var newTrain = $('<tr>');
-                var childName = $("<td>").text(childData.trainName);
-                var childDestination = $("<td>").text(childData.trainDestination);
-                var childTime = $("<td>").text(childData.trainTime);
-                var childFrequency = $("<td>").text(childData.trainFrequency);
+            var newTrain = $('<tr>');
+            var childName = $("<td>").text(childData.trainName);
+            var childDestination = $("<td>").text(childData.trainDestination);
+            var childFrequency = $("<td>").text(childData.trainFrequency + " min");
+            var childTime = $("<td>").text(childData.nextTrainCoverted);
+            var minAway = $("<td>").text(childData.tMinutesTillTrain);
+            var close = $("<td>").html('<i class="fa fa-window-close" aria-hidden="true"></i>');
+            close.addClass('close-btn');
+            close.attr('data-train', key);
+            
+            newTrain.append(childName).append(childDestination).append(childFrequency).append(childTime).append(minAway).append(close);
 
-                newTrain.append(childName).append(childDestination).append(childFrequency).append(childTime);
+            $('#train-schedule-area').append(newTrain);
 
-                $('#train-schedule-area').append(newTrain);
-
-        });
-        
-            // Handle the errors
+        // Handle the errors
         }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
-        
-        });// Firebase Watcher
+        console.log("Errors handled: " + errorObject.code);
+        });
+
     }// print to page
     printToPage();
 
+        // click the x button and delete the train
+        $('body').on('click', '.close-btn', function(){
+            var dataTrain = $(this).attr('data-train');
+            console.log("dataTrain: " + dataTrain);
+            database.ref(dataTrain).remove();
+            $(this).remove();
+            printToPage();
+        });
+
+        // convert string to captialize first letters
+        String.prototype.toProperCase = function () {
+            return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        };
 
 }); // document ready 
